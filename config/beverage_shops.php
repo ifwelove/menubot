f759912c38478ae44bd2a8944059d6e6231ee30f
ponpon <ponpon@flydove.net>
2024-08-28 15:20:47 +0800

    first

--- config/beverage_shops.php
+++ config/beverage_shops.php
@@ -4,7 +4,7 @@ return [
     'shops' => [
         '50嵐' => 'https://photo.518.com.tw/selfmedia/articles/2220/169138987315030.jpeg',
 //        '50嵐' => '/images/menus/50嵐.png',
-        '清心福全' => '/images/menus/清心福全.png',
+        '清心福全' => 'https://twcoupon.com/images/menu/p_chingshin.jpg',
         // 添加更多的飲料店
     ],
 ];
