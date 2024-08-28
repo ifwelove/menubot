7cb10c4886b77e12453aef3ff68277dd0852766e
ponpon <ponpon@flydove.net>
2024-08-28 15:15:16 +0800

    first

--- config/beverage_shops.php
+++ config/beverage_shops.php
@@ -2,7 +2,8 @@
 
 return [
     'shops' => [
-        '50嵐' => '/images/menus/50嵐.png',
+        '50嵐' => 'https://photo.518.com.tw/selfmedia/articles/2220/169138987315030.jpeg',
+//        '50嵐' => '/images/menus/50嵐.png',
         '清心福全' => '/images/menus/清心福全.png',
         // 添加更多的飲料店
     ],
