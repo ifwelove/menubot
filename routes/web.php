6dec0b0cd629a560e7ac066892f46a3b4ee226ec
ponpon <ponpon@flydove.net>
2024-08-28 14:25:47 +0800

    first

--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,12 @@
 use App\Http\Controllers\LineBotController;
 
 Route::post('/webhook', [LineBotController::class, 'webhook']);
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return response()->json([
+        'message' => 'This is a test route',
+        'status' => 'success'
+    ]);
+});
