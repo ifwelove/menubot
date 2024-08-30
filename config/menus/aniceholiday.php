aab14baafabf0f856db8edcde967154141dd714f
ponpon <ponpon@flydove.net>
2024-08-31 02:24:55 +0800

    first

---
+++ config/menus/aniceholiday.php
@@ -0,0 +1,225 @@
+<?php
+return [
+    'brand_code' => 'aniceholiday',
+    'shop_name' => 'ㄧ沐日',
+    'image_url' => 'https://www.aniceholiday.com.tw/static/images/logo.svg?v=20240326',
+    'menu_items' => [
+        '原味茶' => [
+            [
+                'name' => '竹香翡翠',
+                'price' => '40',
+                'image_url' => ''
+            ],
+            [
+                'name' => '招牌紅',
+                'price' => '35',
+                'image_url' => ''
+            ],
+            [
+                'name' => '炭焙烏龍',
+                'price' => '35',
+                'image_url' => ''
+            ],
+            [
+                'name' => '油切蕎麥茶',
+                'price' => '35',
+                'image_url' => ''
+            ],
+            [
+                'name' => '手採高山青',
+                'price' => '35',
+                'image_url' => ''
+            ],
+            [
+                'name' => '桂花蕎麥茶',
+                'price' => '55',
+                'image_url' => ''
+            ],
+        ],
+        '風味茶' => [
+            [
+                'name' => '梅果招牌紅',
+                'price' => '55',
+                'image_url' => ''
+            ],
+            [
+                'name' => '養樂多青茶',
+                'price' => '55',
+                'image_url' => ''
+            ],
+            [
+                'name' => '蜂蜜檸檬蘆薈',
+                'price' => '55',
+                'image_url' => ''
+            ],
+            [
+                'name' => '粉粿黑糖檸檬',
+                'price' => '60',
+                'image_url' => ''
+            ],
+            [
+                'name' => '粉粿桂花檸檬',
+                'price' => '60',
+                'image_url' => ''
+            ],
+            [
+                'name' => '荔枝蘆薈',
+                'price' => '60',
+                'image_url' => ''
+            ],
+            [
+                'name' => '荔枝烏龍',
+                'price' => '55',
+                'image_url' => ''
+            ],
+            [
+                'name' => '蜜香烏龍',
+                'price' => '45',
+                'image_url' => ''
+            ],
+            [
+                'name' => '鮮檸檬紅茶',
+                'price' => '60',
+                'image_url' => ''
+            ],
+            [
+                'name' => '鮮檸檬青茶',
+                'price' => '60',
+                'image_url' => ''
+            ],
+            [
+                'name' => '鮮葡萄柚青茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+            [
+                'name' => '柚子烏龍茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+        ],
+        '奶茶' => [
+            [
+                'name' => '招牌紅奶茶',
+                'price' => '50',
+                'image_url' => ''
+            ],
+            [
+                'name' => '竹香翡翠奶茶',
+                'price' => '50',
+                'image_url' => ''
+            ],
+            [
+                'name' => '烏龍奶茶',
+                'price' => '50',
+                'image_url' => ''
+            ],
+            [
+                'name' => '高山青奶茶',
+                'price' => '50',
+                'image_url' => ''
+            ],
+            [
+                'name' => '黃金蕎麥奶茶',
+                'price' => '50',
+                'image_url' => ''
+            ],
+            [
+                'name' => '逮丸奶茶〔粗吸管〕',
+                'price' => '65',
+                'image_url' => ''
+            ],
+            [
+                'name' => '粉粿黑糖奶茶',
+                'price' => '60',
+                'image_url' => ''
+            ],
+            [
+                'name' => '極黑芝麻奶茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+        ],
+        '芝士奶蓋' => [
+            [
+                'name' => '奶蓋招牌紅茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+            [
+                'name' => '奶蓋竹香茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+            [
+                'name' => '奶蓋烏龍茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+            [
+                'name' => '奶蓋蕎麥茶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+        ],
+        '冬瓜茶' => [
+            [
+                'name' => '招牌冬瓜紅',
+                'price' => '45',
+                'image_url' => ''
+            ],
+            [
+                'name' => '冬瓜青茶',
+                'price' => '45',
+                'image_url' => ''
+            ],
+            [
+                'name' => '冬瓜檸檬茶',
+                'price' => '50',
+                'image_url' => ''
+            ],
+            [
+                'name' => '冬瓜仙草蜜',
+                'price' => '50',
+                'image_url' => ''
+            ],
+        ],
+        '鮮奶茶' => [
+            [
+                'name' => '招牌紅拿鐵',
+                'price' => '70',
+                'image_url' => ''
+            ],
+            [
+                'name' => '烏龍拿鐵',
+                'price' => '70',
+                'image_url' => ''
+            ],
+            [
+                'name' => '黃金蕎麥拿鐵',
+                'price' => '70',
+                'image_url' => ''
+            ],
+            [
+                'name' => '竹香拿鐵',
+                'price' => '70',
+                'image_url' => ''
+            ],
+            [
+                'name' => '粉粿黑糖紅拿鐵',
+                'price' => '80',
+                'image_url' => ''
+            ],
+            [
+                'name' => '波霸黑糖燕麥奶',
+                'price' => '65',
+                'image_url' => ''
+            ],
+            [
+                'name' => '極黑芝麻拿鐵',
+                'price' => '80',
+                'image_url' => ''
+            ],
+        ]
+    ]
+];
